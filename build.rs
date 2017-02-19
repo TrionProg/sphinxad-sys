56fd65774563d2fb13a1b59f4a800b8b030913d9
Guillaume Bailleul <laibulle@gmail.com>
2017-02-19 12:22:48 +0100

    Fix build on rust 1.17
    remove circular dependency
    add build dependency

--- build.rs
+++ build.rs
@@ -2,6 +2,5 @@ extern crate pkg_config;
 
 fn main() {
     pkg_config::find_library("pocketsphinx").unwrap();
-    pkg_config::find_library("sphinxad").unwrap();
     pkg_config::find_library("sphinxbase").unwrap();
 }
